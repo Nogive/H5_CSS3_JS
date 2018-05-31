17bbca977a0439c813ee7f3d5031339ec11254c0
chendan <603446367@qq.com>
2018-05-31 14:31:20 +0800

    task 完善

--- vue/task/js/task.js
+++ vue/task/js/task.js
@@ -12,37 +12,73 @@ var list = [
     finished: false
   }
 ];
+Vue.directive("focus", {
+  update: function(el, binding) {
+    if (binding.value) {
+      el.focus();
+    }
+  }
+});
 new Vue({
   data() {
     return {
       list: list,
-      title: ""
+      title: "",
+      editItem: "",
+      oldTitle: "",
+      oldList: list,
+      currentTab: "all"
     };
   },
   computed: {
     total: function() {
-      let i = 0;
-      this.list.forEach(e => {
-        if (!e.finished) {
-          i++;
-        }
-      });
-      return i;
+      return this.list.filter(function(item) {
+        return !item.finished;
+      }).length;
     }
   },
   methods: {
     add() {
       if (this.title != "") {
-        list.push({
+        this.list.push({
           title: this.title,
           finished: false
         });
       }
       this.title = "";
+      oldList = this.list;
     },
     remove(item) {
       let idx = this.list.indexOf(item);
       this.list.splice(idx, 1);
+      oldList = this.list;
+    },
+    preEdit(item) {
+      this.editItem = item;
+      this.oldTitle = item.title;
+    },
+    editTask(item) {
+      this.editItem = "";
+    },
+    cancleEdit(item) {
+      item.title = this.oldTitle;
+      this.editItem = "";
+    },
+    allList() {
+      this.list = this.oldList;
+      this.currentTab = "all";
+    },
+    notFinished() {
+      this.list = this.oldList.filter(function(item) {
+        return !item.finished;
+      });
+      this.currentTab = "notFinished";
+    },
+    finished() {
+      this.list = this.oldList.filter(function(item) {
+        return item.finished;
+      });
+      this.currentTab = "finished";
     }
   }
 }).$mount("#app");

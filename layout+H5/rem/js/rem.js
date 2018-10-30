42ceb2be19d5dcaf3044025171e0a0cc505c0a91
chendan <603446367@qq.com>
2018-10-30 14:57:40 +0800

    rem

---
+++ layout+H5/rem/js/rem.js
@@ -0,0 +1,5 @@
+var _html = document.getElementsByTagName("html")[0];
+var ch = document.documentElement.clientWidth;
+ch > 640
+  ? (_html.style.fontSize = "40px")
+  : (_html.style.fontSize = ch / 16 + "px");
